5bcae1f0de7928411c1769ff5f9c4792748c0538
Mfitumukiza Peter <mfitep6@gmail.com>
2022-02-18 08:00:10 +0200

    some changes

--- Data Structures/stack_class.cpp
+++ Data Structures/stack_class.cpp
@@ -4,49 +4,57 @@ using namespace std;
  
 class Stack {
 	private:
-int stack[SIZE];
-    	int top;
+	int stack[SIZE];
+    int top;
 	public:
     	Stack(){
-  	  top=-1;
-    }
+  	  		top=-1;
+    	}
       
 public:
- int  isEmpty(){
-   	 if(top==-1)
-    	return 1;
-   	 else
-    	return 0;   
+ 	int  isEmpty(){
+   	 	if(top==-1)
+    		return 1;
+   	 	else
+    		return 0;   
     }
- int isFull(){
-	if(top==(SIZE-1))
-    	return 1;
-	else
-    	return 0;
-    	}
- void push(int data){
-
-	if(isFull()){
-    	return ;
+	int isFull(){
+		if(top==(SIZE-1))
+			return 1;
+		else
+			return 0;
 	}
-	++top;
-	stack[top]=data;
+	void push(int data){
+		if(isFull()){
+			cout<<"Stack is full"<<endl;
+			return ;
+		}
+		++top;
+		stack[top]=data;
+	}
 
+	void pop(){
+		if(isEmpty())
+			return ;
+		--top;
+	}
+
+	void display(){
+		cout<<"Stack Values: ";
+		for(int i=top; i>=0; i--)
+			cout<<stack[i]<<" ";
+		cout<<endl;
+	}
+	int peek(){
+		if (top < 0) {
+			cout << "Stack is Empty";
+			return INT_MIN;
+		}
+		else {
+			return stack[top];
+    }
 }
 
- void pop(){
-	if(isEmpty())
-    	return ;
-	--top;
-}
-
- void display(){
-
-	cout<<"Stack Values: ";
-	for(int i=top; i>=0; i--)
-    	cout<<stack[i]<<" ";
-	cout<<endl;
-}
 };
 
 int main(){
